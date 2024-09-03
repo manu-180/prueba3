0f492d8de756f4d169459c9ea8a7612d3ac77e04
manu-180 <manumanu97@hotmail.com>
2024-09-03 02:32:26 -0300

    de cheto

--- rxconfig.py
+++ rxconfig.py
@@ -3,8 +3,9 @@ import reflex as rx
 config = rx.Config(
     app_name="prueba3",
     api_url="https://prueba3-production-4c39.up.railway.app/",
-    # cors_allowed_origins = [
-    #     "http://localhost:3000/",
-    #     "https://baackend.com/"
-    # ]
+    cors_allowed_origins = [
+    "http://localhost:3000",
+    "https://baackend.com",
+    "https://prueba3-production-4c39.up.railway.app",
+]
 )
